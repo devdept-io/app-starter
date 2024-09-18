4abf2f5455c34c9638e75aeae9bf6f2e586a9a0f
Aron Martin <mail@aronmartin.com>
2024-09-18 10:10:28 +0200

    chore: update tauri to rc16

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { internalIpV4 } from 'internal-ip';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-
-const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -26,15 +23,11 @@ export default defineConfig(async () => ({
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['..'],
+    },
     strictPort: true,
-    host: mobile ? '0.0.0.0' : false,
-    hmr: mobile
-      ? {
-          protocol: 'ws',
-          host: await internalIpV4(),
-          port: 1421,
-        }
-      : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ['**/src-tauri/**'],
