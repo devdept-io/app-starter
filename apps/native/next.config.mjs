4982d8843e908bd865a3fffca77ee5bd05885d07
Aron Martin <mail@aronmartin.com>
2024-09-20 13:01:54 +0200

    chore: adjust next-configs

--- apps/native/next.config.mjs
+++ apps/native/next.config.mjs
@@ -1,21 +1,41 @@
+// @ts-check
+
+// Import necessary functions from NX using ES module syntax
+import { composePlugins, withNx } from '@nx/next';
+
+// Define whether the app is in production mode
 const isProd = process.env.NODE_ENV === 'production';
 
+// Define the internal host, use `localhost` if `TAURI_DEV_HOST` is not provided
 const internalHost = process.env.TAURI_DEV_HOST || 'localhost';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions & import('next').NextConfig}
+ **/
+const baseConfig = {
   // Ensure Next.js uses SSG instead of SSR
-  // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
   output: 'export',
+
   // Note: This feature is required to use the Next.js Image component in SSG mode.
-  // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
   images: {
     unoptimized: true,
   },
+
+  // Specify the directory for Next.js output
   distDir: 'apps/native/.next',
 
-  // Configure assetPrefix or else the server won't properly resolve your assets.
+  // Configure assetPrefix depending on the environment
   assetPrefix: isProd ? '' : `http://${internalHost}:3000`,
+
+  // NX-specific configurations
+  nx: {
+    // Set this to true if you would like to use SVGR
+    svgr: false,
+  },
 };
 
-export default nextConfig;
+// List of plugins to apply (in this case, only withNx for now)
+const plugins = [withNx];
+
+// Export the configuration using ES module export syntax
+export default composePlugins(...plugins)(baseConfig);
--- apps/web-app/next.config.mjs
+++ apps/web-app/next.config.mjs
@@ -1,7 +1,7 @@
-//@ts-check
+// @ts-check
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
+// Import necessary functions from NX using ES module syntax
+import { composePlugins, withNx } from '@nx/next';
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -14,9 +14,11 @@ const nextConfig = {
   },
 };
 
+// List of plugins to apply (in this case, only withNx for now)
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
 ];
 
-module.exports = composePlugins(...plugins)(nextConfig);
+// Export the configuration using ES module export syntax
+export default composePlugins(...plugins)(nextConfig);
